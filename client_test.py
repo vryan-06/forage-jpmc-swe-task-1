79722b3fc69632458283f43a02932063aa999fbd
V Ryan <ryan19vt@gmail.com>
2023-03-20 17:08:07 +0530

    added unit testing for ratio

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -23,7 +23,13 @@ class ClientTest(unittest.TestCase):
        self.assertEqual(getDataPoint(q), (q['stock'], q['top_bid']['price'], q['top_ask']['price'], (q['top_bid']['price']+q['top_ask']['price'])/2))
 
   """ ------------ Add more unit tests ------------ """
-
+  
+  def test_getRatio_priceBisZero(self):
+     quotes=[{'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 0, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+      ]
+          
+     self.assertEqual(getRatio(quotes[0]['top_ask']['price'], quotes[1]['top_ask']['price']), "Error")
 
 
 if __name__ == '__main__':
